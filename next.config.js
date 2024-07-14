d2274011ead0aebc35aee217609043f99dec7411
MuhammadZaki <144696915+Mzaki-dev@users.noreply.github.com>
2024-07-14 20:03:52 +0500

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,18 @@
+// next.config.js
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: "export",
+// };
+
+// module.exports = nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
+  output: "export",
+};
+
+module.exports = nextConfig;
